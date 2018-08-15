5e39a5a9fe66bc90e277ca56437cdaf47d5992c3
Omer <omr@omrs-Mac-Pro.local>
2018-08-15 11:47:42 +0300

    Cleanup 3

--- ESP8266/configuration.sample.h
+++ ESP8266/configuration.sample.h
@@ -1,15 +1,15 @@
 // firebase settings
-#define FIREBASE_HOST     "ss-e4daf.firebaseio.com"
-#define FIREBASE_AUTH     "ss"
+#define FIREBASE_HOST     "<firebase-app-id>.firebaseio.com"
+#define FIREBASE_AUTH     "<firebase-db-auth-token>"
 
 // wifi settings
-#define WIFI_SSID         "ss"
+#define WIFI_SSID         ""
 #define WIFI_PASSWORD     ""
 
 // device settings
-#define treshold          3.0
+#define TRESHOLD          3.0
 
 // pushover settings
-#define USE_PUSHOVER      true
-#define PUSHOVER_APP_KEY  "ss"
-#define PUSHOVER_USER_KEY "ss"
+#define USE_PUSHOVER      false
+#define PUSHOVER_APP_KEY  ""
+#define PUSHOVER_USER_KEY ""
