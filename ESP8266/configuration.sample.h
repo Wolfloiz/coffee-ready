dbf2614bbabec69378d08626e8a9a5db9772d2b1
Omer <omr@omrs-Mac-Pro.local>
2018-08-15 10:44:54 +0300

    added configuration file for esp8266 sketch

---
+++ ESP8266/configuration.sample.h
@@ -0,0 +1,15 @@
+// firebase settings
+#define FIREBASE_HOST     "ss-e4daf.firebaseio.com"
+#define FIREBASE_AUTH     "ss"
+
+// wifi settings
+#define WIFI_SSID         "ss"
+#define WIFI_PASSWORD     ""
+
+// device settings
+#define treshold          3.0
+
+// pushover settings
+#define USE_PUSHOVER      true
+#define PUSHOVER_APP_KEY  "ss"
+#define PUSHOVER_USER_KEY "ss"
